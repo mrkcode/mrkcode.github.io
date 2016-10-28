04d5d0c5130f0dd7faddc2c9916be02dda2dc25e
Mark Alterman <mrk.alterman@gmail.com>
2016-10-28 17:54:09 +0300

    add template

---
+++ templates/7/gulpfile.js
@@ -0,0 +1,69 @@
+var gulp         = require('gulp'),
+	browserSync  = require('browser-sync'),
+	sass         = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	concat       = require('gulp-concat'),
+	uglify       = require('gulp-uglifyjs');
+
+gulp.task('browser-sync', function(){
+	browserSync({
+		server: {
+			baseDir: './'
+		},
+		notify: false
+	});
+});
+
+gulp.task('sass', function(){
+	gulp
+		.src('sass/**/*.sass')
+		.pipe(sass({
+			outputStyle: 'expanded',
+			includePaths: [require('node-bourbon').includePaths]
+		}))
+		.on('error', sass.logError)
+		.pipe(autoprefixer({browsers: ['last 25 versions'], cascade: false}))
+		.pipe(gulp.dest('css'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('cLibsJs', function(){
+	gulp.src([
+		'libs/jquery/dist/jquery.min.js',
+		//'libs/magnific-popup/dist/jquery.magnific-popup.min.js'
+	])
+	.pipe(concat('libs.js'))
+	//.pipe(uglify())
+	.pipe(gulp.dest('js'));
+});
+
+gulp.task('cLibsCss', function(){
+	gulp.src([
+		'libs/font-awesome/css/font-awesome.min.css'
+	])
+	.pipe(concat('libs.css'))
+	//.pipe(uglify())
+	.pipe(gulp.dest('css'));
+});
+
+/*
+gulp.task('compileJsIe', function(){
+	gulp.src([
+		"libs/es5-shim/es5-shim.min.js",
+		"libs/html5shiv/dist/html5shiv.min.js",
+		"libs/html5shiv/dist/html5shiv-printshiv.min.js",
+		"libs/respond/dest/respond.min.js"
+	])
+	.pipe(concat('ie-compiled.js'))
+	//.pipe(uglify())
+	.pipe(gulp.dest('js'));
+});
+*/
+
+gulp.task('start', ['sass', 'browser-sync'], function(){
+	gulp.watch(['sass/**/*.sass'], ['sass']);
+
+	gulp.watch('js/**/*.js').on('change', browserSync.reload);
+	gulp.watch('img/**/*').on('change', browserSync.reload);
+	gulp.watch('*.html').on('change', browserSync.reload);
+})
